004e4d76528cc7a7c4493097f3eccd0fe06ed3e1
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2023-08-23 22:55:48 +0300

    В файл Main сториборда были добавлены Label и Button. Также были заданы начальные тексты к ним. В файле ViewController были добавлены связь Label и Button с кодом, а также функциональность для кнопки, которая при нажатии добавляет 1 к текущему значению счётчика.

--- CouterApp/ViewController.swift
+++ CouterApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var couterLabel: UILabel!
+    
+    private var couter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAddNumber(_ sender: Any) {
+        couter += 1
+        couterLabel.text = "Значение счётчика: \(couter)"
+    }
+    
 }
 
