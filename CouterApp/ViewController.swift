24d532d8f41010fe314a1b220b786dbe3d2fda88
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2023-08-26 21:06:35 +0300

    Были добавлены модификаторы доступа private к IBOutlet и IBAction. Была вынесена реализация кнопки в отдельную функцию. Также код был очищен от лишних комментариев.

--- CouterApp/ViewController.swift
+++ CouterApp/ViewController.swift
@@ -1,24 +1,20 @@
-//
-//  ViewController.swift
-//  CouterApp
-//
-//  Created by Alexandr Artemov (Mac Mini) on 23.08.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var couterLabel: UILabel!
+    @IBOutlet weak private var couterLabel: UILabel!
     
     private var couter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-    @IBAction func buttonAddNumber(_ sender: Any) {
+    @IBAction private func buttonAddNumber(_ sender: Any) {
+        incrementCouter()
+    }
+    
+    private func incrementCouter() {
         couter += 1
         couterLabel.text = "Значение счётчика: \(couter)"
     }
